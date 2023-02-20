bfb44e25c575cdcda04d4a14bad38338016e315f
your name <hello@xample.com>
2023-02-20 11:41:33 -0500

    added headers for reporting

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use App\Models\transaction;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ReportExport implements FromCollection
+class ReportExport implements FromCollection, withHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,13 @@ class ReportExport implements FromCollection
     {
         return transaction::all();
         //
+    }
+    public function headings(): array {
+
+        return[
+            'id', 'puid', 'meal_date', 'week_no', 'location_id', 'location_name', 'mealperiod', 'meal_day',
+            'host_userid', 'host_puid', 'host_name', 'guest_userid', 'guest_puid', 'guest_name', 'approved',
+            'status', 'comments', 'processed', 'manual_entry', 'entry_userid', 'created_at', 'updated_at'
+        ];
     }
 }
